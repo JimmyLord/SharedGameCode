12dee90b85b67c52a011b16a204235e39a2dee92
Jimmy Lord <jimmylord@gmail.com>
2016-06-06 18:05:48 -0400

    Changed PCHs again, forced include caused issues with edit and continue.

--- Menus/MenuText.cpp
+++ Menus/MenuText.cpp
@@ -7,11 +7,7 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
-//#ifndef PCHFILE
-//#define PCHFILE "EngineCommonHeader.h"
-//#endif
-
-#include "../../Framework/MyFramework/SourceCommon/CommonHeader.h"
+#include "SharedCommonHeader.h"
 #include "../Core/RenderTextQuick.h"
 #include "../Core/MeshShapes.h"
 #include "MenuText.h"

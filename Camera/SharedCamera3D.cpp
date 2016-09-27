e6637ad3300b90837da4c06cb3b169242d21b049
Jimmy Lord <jimmylord@gmail.com>
2016-09-27 08:01:50 -0400

    Updates to match Framework CreateLookAtView changes.

--- Camera/SharedCamera3D.cpp
+++ Camera/SharedCamera3D.cpp
@@ -38,11 +38,7 @@ void SharedCamera3D::LookAt(Vector3& eye, Vector3& up, Vector3& lookatpos)
 
 void SharedCamera3D::ResetMatrix(bool calculateinverse)
 {
-#if MYFW_RIGHTHANDED
     m_matView.CreateLookAtView( m_Eye, m_Up, m_LookAtPos );
-#else
-    m_matView.CreateLookAtViewLeftHanded( m_Eye, m_Up, m_LookAtPos );
-#endif
 
     m_matViewProj = m_matProj * m_matView;
 
